201868654bc844b6c0fad36db1a73bde2233dfd6
ajay6915 <67999037+ajay6915@users.noreply.github.com>
2022-12-05 20:17:57 +0100

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -8,7 +8,7 @@ elif [ -f /bin/pacman ]; then
 else echo "fzf, zoxide and thefuck have to be installed"
 fi
 
-echo -e "\n######################################\n#  Installing oh-my-zsh  #\n######################################\n"
+echo -e "\n######################################\n#  Installing oh-my-zsh   #\n######################################\n"
 sleep 1
 git clone https://github.com/ohmyzsh/ohmyzsh.git
 sed -i 's/  exec zsh -l//' ./ohmyzsh/tools/install.sh
@@ -16,7 +16,7 @@ sleep 1
 sh ./ohmyzsh/tools/install.sh --skip-chsh
 rm -rf ohmyzsh
 
-echo -e "\n######################################\n#      Installing zsh-plugins       #\n######################################\n"
+echo -e "\n######################################\n#      Installing zsh-plugins        #\n######################################\n"
 cd /home/$USER/.oh-my-zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone https://github.com/zsh-users/zsh-history-substring-search.git
@@ -26,13 +26,14 @@ sed -i 's/plugins=(git)/plugins=(git fzf zoxide thefuck zsh-autosuggestions zsh-
 cd /home/$USER/.oh-my-zsh/themes
 git clone https://github.com/romkatv/powerlevel10k.git 
 sed -i 's|robbyrussell|powerlevel10k/powerlevel10k|' /home/$USER/.zshrc
+cd /home/$USER
 
 sleep 1
 echo -e "\n######################################\n#      Changing shell to zsh       #\n######################################\n"
 chsh -s /bin/zsh $USER
 
 
-echo -e "\n######################################\n#  Adding Neofetch to .zshrc   \n######################################\n"
+echo -e "\n######################################\n#    Adding Neofetch to .zshrc     \n######################################\n"
 sleep 1
 echo "neofetch" >> ~/.zshrc
 #sudo reboot
