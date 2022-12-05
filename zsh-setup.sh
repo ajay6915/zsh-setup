3563513c5981819b6ac5306fafc47564a90f02e8
ajay6915 <67999037+ajay6915@users.noreply.github.com>
2022-12-05 20:13:55 +0100

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-WEISS='\033[1;37m'
-GELB='\033[1;33m'
 
-#sudo apt install -y git wget curl zsh neovim neofetch
+if [ -f /bin/apt ]; then
+	sudo apt install -y fzf zoxide thefuck
+elif [ -f /bin/pacman ]; then
+	sudo pacman -S fzf zoxide thefuck
+else echo "fzf, zoxide and thefuck have to be installed"
+fi
 
-echo -e "\n$WEISS######################################\n#$GELB  Installiere oh-my-zsh  $WEISS#\n######################################\n"
+echo -e "\n######################################\n#  Installing oh-my-zsh  #\n######################################\n"
 sleep 1
 git clone https://github.com/ohmyzsh/ohmyzsh.git
 sed -i 's/  exec zsh -l//' ./ohmyzsh/tools/install.sh
@@ -13,23 +16,23 @@ sleep 1
 sh ./ohmyzsh/tools/install.sh --skip-chsh
 rm -rf ohmyzsh
 
-echo -e "\n$WEISS######################################\n#$GELB      Installiere zsh-plugins       $WEISS#\n######################################\n"
-cd ~/.oh-my-zsh/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $USER/.ohmyzsh/plugins/
-git clone https://github.com/zsh-users/zsh-history-substring-search.git $USER/.ohmyzsh/plugins/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $USER/.ohmyzsh/plugins/
+echo -e "\n######################################\n#      Installing zsh-plugins       #\n######################################\n"
+cd /home/$USER/.oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+git clone https://github.com/zsh-users/zsh-history-substring-search.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 sed -i 's/plugins=(git)/plugins=(git fzf zoxide thefuck zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)/' ~/.zshrc
 
-
-git clone https://github.com/romkatv/powerlevel10k.git $USER/.ohmyzsh/themes/
-sed -i 's|robbyrussell|powerlevel10k/powerlevel10k|' $USER/.zshrc
+cd /home/$USER/.oh-my-zsh/themes
+git clone https://github.com/romkatv/powerlevel10k.git 
+sed -i 's|robbyrussell|powerlevel10k/powerlevel10k|' /home/$USER/.zshrc
 
 sleep 1
-echo -e "\n$WEISS######################################\n#$GELB      Stelle Shell auf zsh um       $WEISS#\n######################################\n"
+echo -e "\n######################################\n#      Changing shell to zsh       #\n######################################\n"
 chsh -s /bin/zsh $USER
 
 
-echo -e "\n$WEISS######################################\n#$GELB  Richte Startoption Neofetch ein   $WEISS#\n######################################\n"
+echo -e "\n######################################\n#  Adding Neofetch to .zshrc   \n######################################\n"
 sleep 1
 echo "neofetch" >> ~/.zshrc
 #sudo reboot
